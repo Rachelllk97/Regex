44a4765198d269d9a5ef8153ced224afa26a81c5
Alice Barrett <alice.barrett@softwire.com>
2025-04-23 20:46:39 +0100

    finishing off for practice : dictionary

--- regex.js
+++ regex.js
@@ -3,8 +3,9 @@ import fs from 'fs';
 function readFile (){
     fs.readFile('test.txt', 'utf8', (err, data) => {
         if (err) throw err;
-        var input = data
-        useDictionary(input)
+        var input = data;
+        useDictionary(input);
+        topTen
     });
 }
 
@@ -21,17 +22,17 @@ function findString (input) {
 
 
 function useRegex (input) {
-    let email_domain = "@softwire.com"
-    let regex = /[A-Za-z0-9._%+-]+@softwire\.com/g
-    let found = input.match(regex)
-    let count = found.length
+    let email_domain = "@softwire.com";
+    let regex = /[A-Za-z0-9._%+-]+@softwire\.com/g;
+    let found = input.match(regex);
+    let count = found.length;
 
 }
 
 function findCharIndex(email, char) {
     for (let i = 0; i < email.length; i++){
         if(email[i] === "\@"){
-            return email.indexOf(i)
+            return i;
     }
 
 }
@@ -39,12 +40,20 @@ function findCharIndex(email, char) {
 
 function useDictionary (input) {
     let dictionary = {};
-    let regex = /(\w[\w\.-]*\w@[a-zA-Z\d\.-]+\.[a-zA-Z]+)/g
-    let found = input.match(regex)
-    let count = found.length
+    let regex = /(\w[\w\.-]*\w@[a-zA-Z\d\.-]+\.[a-zA-Z]+)/g;
+    let found = input.match(regex);
+    let count = found.length;
     found.forEach((email, i) => {
-        findCharIndex(email, i)
+        let domainIndex = findCharIndex(email, i)+1;
+        let domain = email.substring(domainIndex,email.length);
+        if (dictionary[domain]) {
+            dictionary[domain] ++;
+        } else {
+            dictionary[domain] = 1;
+        }
+         
     })
+    console.log(dictionary);
 }
 
 //found index of @
@@ -53,7 +62,6 @@ function useDictionary (input) {
 //push remainig characters to an object 
 //add a frequency count per characters 
 //check if characters exist in object before adding or increasing frequency count 
-
 
 
 readFile()
