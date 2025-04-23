24bb8df0c462d84fd9bd554063a82fa1381aad4f
Alice Barrett <alice.barrett@softwire.com>
2025-04-23 14:50:39 +0100

    Using dictionary to log email addresses

--- regex.js
+++ regex.js
@@ -4,7 +4,7 @@ function readFile (){
     fs.readFile('test.txt', 'utf8', (err, data) => {
         if (err) throw err;
         var input = data
-        useRegex(input)
+        useDictionary(input)
     });
 }
 
@@ -28,6 +28,19 @@ function useRegex (input) {
     let count = found.length
 
 console.log(count)
+console.log(found);
+}
+
+function useDictionary (input) {
+    let dictionary = {};
+    //let regex = /[A-Za-z0-9._%+-]+@+[A-Za-z]\.com/g
+    let regex = /(\w[\w\.-]*\w@[a-zA-Z\d\.-]+\.[a-zA-Z]+)/g
+    let found = input.match(regex)
+    let count = found.length
+    console.log(count)
+    console.log(found)
+
+
 }
 
 readFile()
