7cc0e1cd90ea37f82b478fcdaf41cbb318172a6a
Rachel Kelly <rachel.kelly@softwire.com>
2025-04-23 11:51:04 +0100

    read file and count emails containing softwire

--- regex.js
+++ regex.js
@@ -1,24 +1,27 @@
-const fs = require('fs')
+import fs from 'fs';
+
+function readFile (){
+
+fs.readFile('test.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  var input = data
+  findString(input)
+});
+
+}
 
 
-var input = fs.readFile('test.txt')
-
-
-// function readFile () {
-
+function findString (input) {
 var counter = 0
-
  
 for(var i = 0; i < input.length; i++) {
-  if ((input.substring(i, 13) == '@softwire.com')) { counter = counter + 1 }
+  if ((input.substring(i, i + 13 ) === '@softwire.com')) { 
+    counter = counter + 1 }
 }
 console.log(counter)
 
-// }
+}
 
-import { readFile } from 'node:fs';
+readFile()
 
-readFile('/etc/passwd', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+
